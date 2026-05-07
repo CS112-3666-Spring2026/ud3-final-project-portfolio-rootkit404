6107786cec99e42725b85cbb752ba9b9e7d1e19d
Nathan Tshishimbi <149440288+nathanltish@users.noreply.github.com>
2026-05-07 17:42:19 +0000

    commit 1

---
+++ PlayingCard.java
@@ -0,0 +1,13 @@
+public class PlayingCard {
+
+    // ENUMERATION CONSTANTS
+    public enum Rank {
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+
+    public enum Suit {
+        HEARTS, DIAMONDS, CLUBS, SPADES
+    }
+    
+    
+}
